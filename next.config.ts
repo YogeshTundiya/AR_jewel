ec4fc60eddc3dea1e356e5169ba225d12f69662c
YogeshTundiya <yogeshtundiya945@gmail.com>
2026-05-04 11:23:45 +0530

    Fix Turbopack compilation by adding three to transpilePackages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: ["three", "@react-three/drei", "@react-three/fiber"],
 };
 
 export default nextConfig;
